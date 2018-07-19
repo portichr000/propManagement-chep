06eea88aea5521e4b85aff6e01333a7baee1b947
Christian <chris6por@gmail.com>
2018-07-19 15:09:53 +0000

    added callback to tabnave handletabChange

--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 
 class TabNav extends Component {
   render() {
-      return(
-          <div className='tab-nav'>
-                <div className='tab-nav__tabs'>
-                    {
-                        this.props.tabs.map((tab, index) => {
-                            return <a className='tab-nav__tab'>{tab.title}</a>
-                        })
+    return (
+        <div className='tab-nav'>
+            <div className='tab-nav__tabs'>
+            {
+                this.props.tabs.map((tab, index) => {
+                    return <a key={index} onClick={() => this.props.handleClick(tab.title)} className='tab-nav__tab'>{tab.title}</a>
+                })
+            }
+            </div>
+            {
+                this.props.tabs.map((tab, index) => {
+                    if(tab.active) {
+                    return(
+                        <div key={index} className='tab-nav__componet'>
+                            { tab.component }
+                        </div>
+                    )
                     }
-                </div>
-                <div>requests or newsletters content goes here</div>
-          </div>
-      );
+                })
+            }
+            
+        </div>
+    )
   }
 }
 
