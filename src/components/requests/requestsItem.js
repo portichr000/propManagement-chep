b343b500dafd8aaf654cdc79c24f6de84d5ddce1
Christian <chris6por@gmail.com>
2018-07-26 15:46:03 +0000

    built requests item component base and appied grid to requestsitem

---
+++ src/components/requests/requestsItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Button from '../button';
+
+import RequestsBoxes from './requestsBoxes';
+import Requests from './requests';
+
+class RequestsItem extends Component {
+    
+    render() {
+        return (
+            <div className='requests-item'>
+                itme goees here
+            </div>
+        )
+    }
+}
+
+export default RequestsItem;
