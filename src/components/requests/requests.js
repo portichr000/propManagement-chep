2818ac7f43753a7710a80853d7719ef6621886a1
Christian <chris6por@gmail.com>
2018-07-31 14:35:29 +0000

    created type to set requests and reducer to handle said type

--- src/components/requests/requests.js
+++ src/components/requests/requests.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 import RequestsItem from './requestsItem'
 
 class Requests extends Component {
     render() {
         return (
             <div className='requests'>
-                <RequestsItem/>
-                <RequestsItem/>
-                <RequestsItem/>
+                {
+                    this.props.requests.map(requestItem => {
+                        return <RequestsItem {...requestItem._id}/>
+                    })
+                }
             </div>
         )
     }
 }
 
-export default Requests;
+function mapStateToProps(state) {
+    const { requests } = state.requests;
+    return {
+        requests
+    }
+}
+
+export default connect(mapStateToProps)(Requests);
